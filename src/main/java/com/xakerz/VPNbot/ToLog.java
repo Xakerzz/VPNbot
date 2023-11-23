a762ad7a09403448c301cabda30d2ae414c34b91
xakerzz <xakerz@me.com>
2023-11-23 18:42:46 +0300

    Commit The app is done

--- src/main/java/com/xakerz/VPNbot/ToLog.java
+++ src/main/java/com/xakerz/VPNbot/ToLog.java
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ToLog {
-    private static final String LOG_FILE = "src/main/resources/Files/Log.txt";
+    private static final String LOG_FILE = "/root/VPNbot/VPNbot/src/main/resources/Files/Log.txt";
 
     public static void log(String message, Long chatId, String userNameFirstName) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE, true))) {
